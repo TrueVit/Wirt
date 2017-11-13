0ed414eae400f6997af20b98c142d61d6fe193f6
True Vit <truevit@gmail.com>
2017-11-13 09:56:29 +0400

    Commit 3
    Test: From PC to Laptop

--- Wirt/Program.cs
+++ Wirt/Program.cs
@@ -11,6 +11,7 @@ namespace Wirt
         static void Main(string[] args)
         {
             Console.WriteLine("Test!!!");
+            Console.WriteLine("FromMainToNotebook");
         }
     }
 }
