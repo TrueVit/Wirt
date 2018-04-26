77e15488991f373e9c1c6eccc69865dfd6e9f5fc
True Vit <truevit@gmail.com>
2018-04-26 10:18:54 +0400

    Add login

--- Wirt/Program.cs
+++ Wirt/Program.cs
@@ -20,9 +20,16 @@ namespace Wirt
             driver.Url = "https://virtonomica.ru";
             IWebElement elem = driver.FindElement(By.ClassName("dialog_login_opener"));
             elem.Click();
+            driver.FindElement(By.Id("mail_or_login_field")).SendKeys("truevit");
+            driver.FindElement(By.XPath("//*[@id=\"old\"]/form/label[2]/input")).SendKeys("wo7Kg0oB");
+            driver.FindElement(By.XPath("//*[@id=\"old\"]/form/input[1]")).Click();
+            driver.Url = "https://virtonomica.ru/vera/main/company/view/4785020/unit_list";
+            driver.FindElement(By.XPath("//*[@id=\"mainContent\"]/table/tbody/tr[1]/td/a[3]")).Click();
+            
             //string page_text = driver.PageSource;
             //Console.WriteLine(page_text);
-            //driver.Quit();
+            Console.ReadLine();
+            driver.Quit();
         }
     }
 }
