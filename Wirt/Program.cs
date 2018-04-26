2e72cfb8c1972c2cd20356f8d7d2b530e8958680
TrueVit <truevit@gmail.com>
2018-04-26 12:26:15 +0300

    Add techonlogies select

--- Wirt/Program.cs
+++ Wirt/Program.cs
@@ -17,14 +17,31 @@ namespace Wirt
             Console.WriteLine("FromMainToNotebook");
             Console.WriteLine("FromLaptopToPC");
             IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
             driver.Url = "https://virtonomica.ru";
+            //driver.Manage().Window.Maximize();
             IWebElement elem = driver.FindElement(By.ClassName("dialog_login_opener"));
             elem.Click();
             driver.FindElement(By.Id("mail_or_login_field")).SendKeys("truevit");
             driver.FindElement(By.XPath("//*[@id=\"old\"]/form/label[2]/input")).SendKeys("wo7Kg0oB");
             driver.FindElement(By.XPath("//*[@id=\"old\"]/form/input[1]")).Click();
-            driver.Url = "https://virtonomica.ru/vera/main/company/view/4785020/unit_list";
-            driver.FindElement(By.XPath("//*[@id=\"mainContent\"]/table/tbody/tr[1]/td/a[3]")).Click();
+            driver.Url = "https://virtonomica.ru/vera/main/management_action/4785020/investigations/technologies";
+            foreach (IWebElement row in driver.FindElements(By.ClassName("tech_row"))  )
+            {
+                try
+                {
+                    IWebElement subelem = row.FindElement(By.ClassName("tech_title_cell"));
+                    Console.WriteLine(subelem.Text);
+                }
+                catch
+                {
+                    //Console.WriteLine("Error!");
+                }
+                //Console.WriteLine(subelem.Text);
+            }
+
+            //driver.Url = "https://virtonomica.ru/vera/main/company/view/4785020/unit_list";
+            //driver.FindElement(By.XPath("//*[@id=\"mainContent\"]/table/tbody/tr[1]/td/a[3]")).Click();
             
             //string page_text = driver.PageSource;
             //Console.WriteLine(page_text);
